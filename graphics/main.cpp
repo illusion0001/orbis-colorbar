219ce6f8227c217caeaddbaf203b77fff5c71296
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-04-07 23:43:18 -0500

    Update main.cpp

--- graphics/main.cpp
+++ graphics/main.cpp
@@ -46,16 +46,16 @@ int main()
         int bar_height = 1080;
         // Color spec according to docs:
         // https://international.download.nvidia.com/geforce-com/international/pdfs/FCAT+4K_Reviewer's_Guide.pdf
-        Color color0 = { 255, 255, 255 };  // 1.  White
-        Color color1 = { 000, 255, 000 };  // 2.  Lime
-        Color color2 = { 000, 102, 255 };  // 3.  Blue
-        Color color3 = { 255, 000, 000 };  // 4.  Red
-        Color color4 = { 051, 204, 204 };  // 5.  Teal
-        Color color5 = { 000, 051, 204 };  // 6.  Navy
-        Color color6 = { 000, 176, 000 };  // 7.  Green
-        Color color7 = { 000, 255, 255 };  // 8.  Aqua
-        Color color8 = { 138, 135, 000 };  // 9.  Dark Green (Dk Green)
-        Color color9 = { 221, 221, 221 };  // 10. Silver
+        Color color0  = { 255, 255, 255 }; // 1.  White
+        Color color1  = { 000, 255, 000 }; // 2.  Lime
+        Color color2  = { 000, 102, 255 }; // 3.  Blue
+        Color color3  = { 255, 000, 000 }; // 4.  Red
+        Color color4  = { 051, 204, 204 }; // 5.  Teal
+        Color color5  = { 000, 051, 204 }; // 6.  Navy
+        Color color6  = { 000, 176, 000 }; // 7.  Green
+        Color color7  = { 000, 255, 255 }; // 8.  Aqua
+        Color color8  = { 138, 135, 000 }; // 9.  Dark Green (Dk Green)
+        Color color9  = { 221, 221, 221 }; // 10. Silver
         Color color10 = { 153, 000, 204 }; // 11. Purple
         Color color11 = { 185, 172, 003 }; // 12. Olive
         Color color12 = { 119, 119, 119 }; // 13. Gray
